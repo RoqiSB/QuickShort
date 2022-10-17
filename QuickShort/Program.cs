bba802fd7ba901346c5696826f170c353ca09d0d
Roqi SB <roqisb9@gmail.com>
2022-10-17 17:47:26 +0700

    add void display

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -84,6 +84,28 @@ namespace QuickShort
                 swap(low, j);
                 mov_count++;
             }
+            // sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1,high);
+        }
+        void display()
+        {
+            Console.WriteLine("\n---------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-----------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
+        }
+        int getSize()
+        {
+            return (n);
         }
     }
 }
